3e43059b304e60d62429e2116d0bc06b1ee93840
mrblackoutz <107147086+mrblackoutz@users.noreply.github.com>
2024-09-20 01:49:52 +0000

    INIT two

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../theme';
+import { CssBaseline } from "@mui/material";
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,8 +31,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          <ThemeProvider theme={theme}>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
       </body>
+
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
-import Image from "next/image";
+import { Box } from "@mui/material";
 
 export default function Home() {
   return (
-    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start border flex-grow-1 bg-black">
-
-MAIN
+    <Box>
+      <main style={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
+DDD
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        FOOTER
+      <footer>
+
       </footer>
-    </div>
+    </Box>
   );
 }
